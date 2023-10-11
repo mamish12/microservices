1a63234a0f679e711b554387d74b4e70b53488a5
mamish12 <mku20sahu@gmail.com>
2023-10-11 11:30:36 +0530

    first

---
+++ src/test/java/com/mklearning/user/service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mklearning.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
